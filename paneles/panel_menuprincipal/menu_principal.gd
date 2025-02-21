a35cb598eab407fe59ec36ec599bac08ce69c506
rubenhs9 <156606767+rubenhs9@users.noreply.github.com>
2025-02-21 21:02:33 +0100

    Menu Principal y GAME OVER 6️⃣ 6️⃣ 6️⃣

---
+++ paneles/panel_menuprincipal/menu_principal.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_altura_pressed() -> void:
+	get_tree().change_scene_to_file("res://mapa/nivel1/nivel1.tscn")
+
+
+func _on_button_tiempo_pressed() -> void:
+	get_tree().change_scene_to_file("res://mapa/nivel 2/nivel2.tscn")
+
+
+func _on_button_salir_pressed() -> void:
+	get_tree().quit()
