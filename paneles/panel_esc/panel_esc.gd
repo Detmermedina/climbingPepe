6028b8f336e032dd55ee1c3d17d108d1c98ebbce
rubenhs9 <rhsilveira9@gmail.com>
2025-03-02 18:40:10 +0100

    Layouts de paneles corregidos 📟

--- paneles/panel_esc/panel_esc.gd
+++ paneles/panel_esc/panel_esc.gd
@@ -1,13 +1,15 @@
-extends Control
-
-# Botón "Salir al Menú"
-func _on_button_salir_menu_pressed():
-	get_tree().change_scene_to_file("res://paneles/panel_menuprincipal/menu_principal.tscn")  
-
-# Botón "Salir al Escritorio"
-func _on_button_salir_escritorio_pressed():
-	get_tree().quit()  # Cierra el juego
+extends CanvasLayer
 
 # Botón "X" para cerrar el menú
 func _on_button_cerrar_pressed():
 	queue_free()  # Elimina la instancia del menú de confirmación
+
+
+func _on_boton_salir_escritorio_pressed() -> void:
+	get_tree().change_scene_to_file("res://paneles/panel_menuprincipal/menu_principal.tscn")  
+	
+
+
+func _on_boton_salir_menu_pressed() -> void:
+	get_tree().quit() # Replace with function body.
+	
