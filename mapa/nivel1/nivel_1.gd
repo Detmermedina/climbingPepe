88660962632a95165b48573ad47f8e2a31df0dc2
Detmermedina <114088648+Detmermedina@users.noreply.github.com>
2025-03-09 20:51:26 +0100

    cambio de tamaño ventana

---
+++ mapa/nivel1/nivel_1.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	# Establecer el tamaño de la ventana en el nivel 1
+	get_tree().root.set_size(Vector2(650, 648))
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- paneles/panel_menuprincipal/menu_principal.gd
+++ paneles/panel_menuprincipal/menu_principal.gd
@@ -4,6 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	get_tree().root.set_size(Vector2(1150, 600))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
