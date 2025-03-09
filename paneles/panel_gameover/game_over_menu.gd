f5f6ea749ba21e08fed8fad4e9c54957b450f910
rubenhs9 <rhsilveira9@gmail.com>
2025-03-09 20:23:47 +0100

    Funcionalidad botones paneles

--- paneles/panel_gameover/game_over_menu.gd
+++ paneles/panel_gameover/game_over_menu.gd
@@ -9,10 +9,9 @@ func _on_restart_button_pressed() -> void:
 	
 
 
+func _on_pressed_jugar() -> void:
+	get_tree().change_scene_to_file("res://mapa/nivel1/nivel1.tscn") # Replace with function body.
 
-func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://paneles/panel_menuprincipal/menu_principal.tscn")
 
-
-func _on_boton_salir_escritorio_pressed() -> void:
-	pass # Replace with function body.
+func _on_boton_salir_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://paneles/panel_menuprincipal/menu_principal.tscn")# Replace with function body.
--- paneles/panel_victoria/panel_victoria.gd
+++ paneles/panel_victoria/panel_victoria.gd
@@ -4,15 +4,12 @@ extends Node
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://paneles/panel_menuprincipal/menu_principal.tscn")
-
-
 func _on_boton_jugar_de_nuevo_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://mapa/nivel1/nivel1.tscn") # Replace with function body.
+
+func _on_pressed_menu() -> void:
+	get_tree().change_scene_to_file("res://paneles/panel_menuprincipal/menu_principal.tscn") # Replace with function body.
