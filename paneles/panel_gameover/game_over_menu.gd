d3dd8d1027efe03289839a718aa18909ab8aa789
juliobp1234 <juliobarbero978@gmail.com>
2025-03-09 19:06:11 +0100

    Mapa y temporizador

--- paneles/panel_gameover/game_over_menu.gd
+++ paneles/panel_gameover/game_over_menu.gd
@@ -2,8 +2,6 @@ extends CanvasLayer
 
 signal restart
 
-
-
 func _on_restart_button_pressed() -> void:
-	restart.emit()
-	
+	restart.emit()  # Emitir la señal (por si algún otro nodo la usa)
+	get_tree().change_scene_to_file("res://paneles/panel_menuprincipal/menu_principal.tscn")  # Cambiar a la escena principal
